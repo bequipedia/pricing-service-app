e6095e20efa4ae7586f3a4688290051441339ba0
rebeca <bequipedia@gmail.com>
2024-06-14 16:29:25 +0200

    [PRICE-SERVICE-APP] add Price Controller and unit tests

---
+++ src/main/java/com/example/pricingservice/infra/controller/PriceController.java
@@ -0,0 +1,30 @@
+package com.example.pricingservice.infra.controller;
+
+import com.example.pricingservice.application.PriceService;
+import com.example.pricingservice.domain.Price;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@RestController
+public class PriceController {
+    @Autowired
+    private PriceService priceService;
+
+    @GetMapping("/prices")
+    public ResponseEntity<Price> getPrice(
+            @RequestParam("applicationDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime applicationDate,
+            @RequestParam("productId") int productId,
+            @RequestParam("brandId") int brandId) {
+
+        Optional<Price> price = priceService.findApplicablePrice(productId, brandId, applicationDate);
+        return price.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+}
---
+++ src/test/java/com/example/pricingservice/infra/controller/PriceControllerTest.java
@@ -0,0 +1,86 @@
+package com.example.pricingservice.infra.controller;
+
+import com.example.pricingservice.application.PriceService;
+import com.example.pricingservice.domain.Price;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class PriceControllerTest {
+    private MockMvc mockMvc;
+
+    @Mock
+    private PriceService priceService;
+
+    @InjectMocks
+    private PriceController priceController;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(priceController).build();
+    }
+
+    @DisplayName("getPrice returns OK with various dates")
+    @ParameterizedTest(name = "{index} => applicationDate={0}, expectedPrice={1}")
+    @MethodSource("priceProvider")
+    public void shouldReturnOkWhenCallGetPrice(LocalDateTime applicationDate, double expectedPrice) throws Exception {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd-HH.mm.ss");
+        Price price = new Price(null, 1, LocalDateTime.parse("2020-06-14-00.00.00", formatter), LocalDateTime.parse("2020-12-31-23.59.59", formatter), 1, 35455, 0, expectedPrice, "EUR");
+
+        when(priceService.findApplicablePrice(35455, 1, applicationDate)).thenReturn(Optional.of(price));
+
+        mockMvc.perform(get("/prices")
+                        .param("applicationDate", applicationDate.toString())
+                        .param("productId", "35455")
+                        .param("brandId", "1")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.price").value(expectedPrice));
+    }
+
+    @DisplayName("getPrice returns 404 when not found")
+    @Test
+    public void shouldReturnNotFoundWhenGetPriceNotFound() throws Exception {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd-HH.mm.ss");
+        LocalDateTime applicationDate = LocalDateTime.parse("2020-06-14-10.00.00", formatter);
+
+        when(priceService.findApplicablePrice(35455, 1, applicationDate)).thenReturn(Optional.empty());
+
+        mockMvc.perform(get("/prices")
+                        .param("applicationDate", "2020-06-14T10:00:00")
+                        .param("productId", "35455")
+                        .param("brandId", "1")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+    }
+
+    static Stream<Object[]> priceProvider() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd-HH.mm.ss");
+        return Stream.of(
+                new Object[]{LocalDateTime.parse("2020-06-14-10.00.00", formatter), 35.50},
+                new Object[]{LocalDateTime.parse("2020-06-14-16.00.00", formatter), 25.45},
+                new Object[]{LocalDateTime.parse("2020-06-14-21.00.00", formatter), 35.50},
+                new Object[]{LocalDateTime.parse("2020-06-15-10.00.00", formatter), 30.50},
+                new Object[]{LocalDateTime.parse("2020-06-16-21.00.00", formatter), 38.95}
+        );
+    }
+}
