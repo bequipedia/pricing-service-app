b18e110a1a0b4eaeacf847d00c959b297d6f6443
rebeca <bequipedia@gmail.com>
2024-06-16 15:54:46 +0200

    [PRICE-SERVICE-APP] remove jakarta notations from domain

--- src/main/java/com/example/pricingservice/price/domain/Price.java
+++ src/main/java/com/example/pricingservice/price/domain/Price.java
@@ -1,9 +1,5 @@
 package com.example.pricingservice.price.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,31 +12,13 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Price {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Column(name = "brand_id", nullable = false)
     private int brandId;
-
-    @Column(name = "start_date", nullable = false)
     private LocalDateTime startDate;
-
-    @Column(name = "end_date", nullable = false)
     private LocalDateTime endDate;
-
-    @Column(name = "price_list", nullable = false)
     private int priceList;
-
-    @Column(name = "product_id", nullable = false)
     private int productId;
-
-    @Column(name = "priority", nullable = false)
     private int priority;
-
-    @Column(name = "price", nullable = false)
     private double price;
-
-    @Column(name = "curr", nullable = false)
     private String currency;
 }
